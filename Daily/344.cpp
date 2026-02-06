57ca794fc346559a988c5278c578c9e1fdd250db
raghavgupta2412 <rg2412147@gmail.com>
2026-02-06 14:43:21 +0530

    updated code of 344.cpp

--- Daily/344.cpp
+++ Daily/344.cpp
@@ -5,10 +5,7 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         for(int i=0;i<s.size()/2;i++){
-            char k= s[i];
-            s[i]=s[s.size()-i-1];
-            s[s.size()-i-1]=k;
-            // swap(s[i],s[s.size()-i-1]);
+            swap(s[i],s[s.size()-i-1]);
         }
     }
 };
