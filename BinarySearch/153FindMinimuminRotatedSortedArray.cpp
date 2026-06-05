a0f009b05b81b3814a711676711857dbbac1f395
raghavgupta2412 <rg2412147@gmail.com>
2026-06-05 10:08:41 +0530

    leeetcode 153 binary search

--- BinarySearch/153FindMinimuminRotatedSortedArray.cpp
+++ BinarySearch/153FindMinimuminRotatedSortedArray.cpp
@@ -4,34 +4,18 @@ using namespace std;
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int m= INT_MIN;
-        return search(nums,m);
-
-    }
-    int search(vector<int>& nums, int target) {
         int ans =INT_MAX;
         int i = 0;
         int j = nums.size() - 1;
-        while (i <= j) {
+        while (i < j) {
             int m = i + (j - i) / 2;
-            if (nums[m] == target) {
-                return m;                       // m is the index we return
-            }
-            ans = min(ans,nums[m]);
-            if (nums[i] <= nums[m]) {           // left half [i..m] is sorted
-                if (target >= nums[i] && target < nums[m]) {
-                    j = m - 1;                  // target lies in the sorted left
-                } else {
-                    i = m + 1;                  // otherwise look right
-                }
-            } else {                            // right half [m..j] is sorted
-                if (target > nums[m] && target <= nums[j]) {
-                    i = m + 1;                  // target lies in the sorted right
-                } else {
-                    j = m - 1;                  // otherwise look left
-                }
+            if(nums[m]>nums[j]){
+                i=m+1;
+            }else{
+                j=m;
             }
         }
-        return ans;
+        return nums[i];        
     }
+
 };
